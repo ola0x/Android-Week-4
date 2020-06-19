0f24fde5eb15e7cf398f4436c14cbaad0dacf285
Olaislord <rayjat51@gmail.com>
2020-06-19 15:36:01 +0100

    new changes

--- app/src/main/java/com/example/week4/FirstFragment.kt
+++ app/src/main/java/com/example/week4/FirstFragment.kt
@@ -6,7 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import com.example.week4.databinding.FragmentFirstBinding
+import com.example.week4.databinding.ActivityMainBinding
+
 
 /**
  * A simple [Fragment] subclass.
@@ -15,11 +16,10 @@ import com.example.week4.databinding.FragmentFirstBinding
  */
 class FirstFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val binding = DataBindingUtil.inflate<FragmentFirstBinding>(
+    //Inflating and Returning the View with DataBindingUtil
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding: ActivityMainBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_first, container, false)
         return binding.root
     }
